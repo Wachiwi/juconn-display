26df954779f7a46fe5cb7f5181acc542c952b79a
David <dev@dschwarz.codes>
2018-05-13 20:54:28 +0200

    [MOD] change layout to be a little bit more specific for task

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import logo from './logo.png';
+import lampActive from './assets/LightbulbOn.svg'
+import lampInactive from './assets/LightbulbOff.svg'
+import infoActive from './assets/infoActive.svg'
+import infoInactive from './assets/infoInactive.svg'
+
 import './App.css';
 
 // https://github.com/electron/electron/issues/7300
@@ -9,20 +15,56 @@ const fs = electron.remote.require('fs');
 var files = fs.readdirSync('./');
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      elements: [{
+        icon: "None",
+        name: "Test",
+        description:"Test123",
+        info:"i",
+        active: true
+      }, {
+        icon: "None",
+        name: "Test",
+        description:"Test123",
+        info:"i",
+      }],
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className="app bright-background">
+        <div className="app-header dark-background">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to resin-electronjs-react</h2>
+          <div className="headline">
+            <h2>Raum 2-U08</h2>
+          </div>
+          <i className="fa fa-heart"></i>
         </div>
-        <div className="App-intro">
-          <p>
-          To get started, edit <code>src/App.js</code> and save to reload. You have access to all node apis. Here is a list of files in this directory.
-          </p>
-          {
-            files.map((file, key) => {
-              return <pre key={key}>{file}</pre>;
+        <div className="cards">
+                    {
+            this.state.elements.map((el) => {
+              return (
+                <div className={`card white-background ${el.active ? 'active' : ''}`}>
+                  <div className="icon">
+                    <img src={el.active? lampActive : lampInactive} />
+                  </div>
+                  <div className="info">
+                    <img src={el.active? infoActive : infoInactive} />{el.info}
+                  </div>
+                  <div className="card-header">
+                    <div class="card-title">
+                      {el.name}
+                    </div>
+                    <div className="card-subtitle">
+                      {el.description}
+                    </div>
+                  </div>
+                </div>
+              );
             })
           }
         </div>
