1fdb2760caee9dcf92200f6a9e6108375776f12c
David <dev@dschwarz.codes>
2018-05-21 15:58:11 +0200

    [ADD] Add more design

--- display/app/src/components/Home.js
+++ display/app/src/components/Home.js
@@ -36,34 +36,14 @@ export default class Home extends Component {
     return (
       <div className="app">
         <Header/>
-        <div className="cards">
-          {
-            this.state.elements.map((el) => {
-              return (
-                <div className={`card white-background ${el.active ? 'active' : ''}`}>
-                  <div className="icon">
-                    {el.active ? (
-                      <i className="fas fa-2x fa-lightbulb yellow"/>
-                    ) : (
-                      <i className="far fa-2x fa-lightbulb"/>
-                    )}
-                  </div>
-                  <div className="info">
-                    <i className={`fas fa-2x fa-info-circle ${el.active ? 'yellow' : ''}`}/>
-                  </div>
-                  <div className="card-header">
-                    <div className="card-title">
-                      {el.name}
-                    </div>
-                    <div className="card-subtitle">
-                      {el.description}
-                    </div>
+        <div className="app-body">
+          <div className="scenes">
 
-                  </div>
-                </div>
-              );
-            })
-          }
+          </div>
+          <div className="devices">
+
+
+          </div>
         </div>
         <Footer/>
       </div>
