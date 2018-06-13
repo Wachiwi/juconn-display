5fdce0765188336bb54ce7b6d2551cbaf559de8c
ZeroSoulEater <t1@purzner.de>
2018-06-13 20:59:16 +0200

    [ADD] Room functionalty

--- display/src/components/device_control.js
+++ display/src/components/device_control.js
@@ -1,11 +1,60 @@
 import React, {Component} from 'react';
 import {FormControlLabel, Switch} from "@material-ui/core";
+import Slider from '@material-ui/lab/Slider';
 
 export default class DeviceControl extends Component {
   constructor(props) {
     super(props);
     this.device = this.props.device;
+    this.state = this.device.state
   }
+
+  onToggleDevice = () => {
+    this.setState({on: !this.state.on});
+  };
+
+  onSliderChange = (event, value) => this.setState({temperature: value});
+
+
+  renderLight_rgbw = () => {
+    return (
+      <div className="actions">
+        <div className={['action', this.state.on? '':'transparent'].join(' ')}>
+          <span>{this.state.brightness * 100}%</span>
+          <div className={'orange circle'}/>
+        </div>
+        <FormControlLabel
+          control={<Switch checked={this.state.on} onChange={this.onToggleDevice} className={'switch primary'}
+                           color="primary"/>} label={this.state.on ? 'On' : 'Off'} className="action"/>
+      </div>
+    )
+  };
+
+  renderTemperaturControll = () => {
+    return (
+      <React.Fragment>
+        <div className='actions'>
+          <div className='action'>
+            Eingestellte Raumtemperatur {this.state.temperature} {this.state.unit.symbol}
+          </div>
+        </div>
+        <div className={'extra slider'}>
+          <Slider
+            min={this.state.minTemperature}
+            max={this.state.maxTemperature}
+            value={this.state.temperature}
+            onChange={this.onSliderChange}
+            step={1}
+          />
+          <span className='label left'>{this.state.minTemperature} {this.state.unit.symbol}</span>
+          <span className='label right'>{this.state.maxTemperature} {this.state.unit.symbol}</span>
+        </div>
+      </React.Fragment>
+
+
+    )
+  };
+
 
   render() {
     return (
@@ -13,12 +62,14 @@ export default class DeviceControl extends Component {
         <span className="title">
           {this.device.name}
         </span>
-        <div className="actions">
-          <FormControlLabel
-            control={<Switch color="primary"/>} label="Off" className="action"/>
-          <FormControlLabel
-            control={<Switch checked={this.device.state.on} color="primary"/>} label={this.device.state.on?'On':'Off'} className="action"/>
-        </div>
+        {
+          this.device.type === 'light_rgbw' &&
+          this.renderLight_rgbw()
+        }
+        {
+          this.device.type === 'temperatur_controll' &&
+          this.renderTemperaturControll()
+        }
       </div>
     );
   }
--- display/src/pages/Room.js
+++ display/src/pages/Room.js
@@ -6,18 +6,66 @@ export default class Room extends Component {
 
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       devices: [{
-        id: '',
+        id: '1234',
         name: 'Lampe 1',
         description: 'LOLULUOLASD',
         type: 'light_rgbw',
         state: {
           on: false,
-          brightness: 0.7
+          brightness: 0.7,
+          hue: null,
+          saturation: null,
+          color_temperature: null,
+          effect: null
         }
+      },{
+        id: '2009',
+        name: 'Lampe Hinten',
+        description: 'LOLULUOLASD',
+        type: 'light_rgbw',
+        state: {
+          on: true,
+          brightness: 0.3,
+          hue: null,
+          saturation: null,
+          color_temperature: null,
+          effect: null
+        }
+      },{
+        id: '1337',
+        name: 'Raum Temperatur',
+        description: 'Temperatur im Konferenzraum 5',
+        type: 'temperatur_controll',
+        state:{
+          on: false,
+          temperature: 20,
+          minTemperature: 13,
+          maxTemperature: 30,
+          unit: {
+            name: 'Grad',
+            symbol: '°'
+          }
+        }
+      }
+      ],
+      scene:[{
+        name:'Meeting Modus',
+        active: false,
+        devices:[{
+          id:'1234',
+          name:'Lampe 1',
+          type: 'light_rgbw',
+          currentState:{
+            on: false,
+          },
+          newState:{
+            on:true,
+          }
+        }, '2009', '1337']
       }]
     }
   }
@@ -26,15 +74,17 @@ export default class Room extends Component {
   render() {
     return (
       <View single={true}>
+        <div className='scenes'>
+
+        </div>
         <div className="devices">
           <div className="title">
             Einzelsteuerung
           </div>
           <div className="list">
             {this.state.devices.map(device => {
-              return <DeviceControl device={device}/>;
+              return <DeviceControl key={device.id} device={device}/>;
             })}
-
           </div>
         </div>
       </View>
